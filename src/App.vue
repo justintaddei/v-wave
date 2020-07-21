5c498220b9a9780ac41d729a8721c3a99e91f67f
Justin Taddei <justin@justintaddei.com>
2020-07-20 20:38:26 -0500

    New Vue CLI project

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>v-wave</h1>
+    <p>The material-ripple directive for Vue that actually works</p>
+    <p><a href="https://github.com/justintaddei/v-wave">Back to Github</a></p>
+
+    <div class="demo">
+      <div v-wave class="box">Default</div>
+      <div style="color: red;" v-wave class="box">Color</div>
+      <div style="border-radius: 50% 10px" v-wave="{ color: 'yellow' }" class="box">
+        border-radius
+      </div>
+      <div
+        v-wave="{
+          duration: 2,
+          color: 'radial-gradient(closest-side, #3f87a6, #ebf8e1, #f69d3c)',
+          initialOpacity: 0.7,
+          finialOpacity: 0.3,
+          easing: 'cubic-bezier(0,.57,.89,0)'
+        }"
+        class="box"
+      >
+        Custom
+      </div>
+      <div
+        style="border-radius: 50%; color:#fff; position:fixed; bottom: 16px; left: 16px; background: #f90;"
+        v-wave
+        class="box"
+      >
+        Fixed position
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
+  name: 'App'
+}
 </script>
 
 <style lang="scss">
@@ -24,5 +48,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  margin-bottom: 1000px;
+}
+
+.demo {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  padding: 64px 256px;
+
+  .box {
+    margin: 32px;
+    width: 200px;
+    height: 200px;
+    box-shadow: 0 4px 24px rgba(0, 0, 0, 0.15);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VWave from "v-wave";
+
+Vue.use(VWave);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
