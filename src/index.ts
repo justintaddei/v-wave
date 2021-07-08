1995d2327c313ac16285d20d5a413884d42357a2
Justin Taddei <justin@justintaddei.com>
2021-07-07 19:10:32 -0600

    feat: unbind directive
    Resolves #69

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { DEFAULT_PLUGIN_OPTIONS, IVWaveDirectiveOptions, IVWavePluginOptions } from 'src/options'
 import { hooks } from 'src/utils/hookKeys'
 import { wave } from 'src/v-wave'
-import { Plugin } from 'vue'
+import { App, Plugin } from 'vue'
 
-const optionMap = new WeakMap<HTMLElement, Partial<IVWaveDirectiveOptions>>()
+const optionMap = new WeakMap<HTMLElement, Partial<IVWaveDirectiveOptions> | false>()
 
 const VWave = {
-  install(app, globalUserOptions: Partial<IVWavePluginOptions> = {}) {
+  install(app: App, globalUserOptions: Partial<IVWavePluginOptions> = {}) {
     if (this.installed) return
     this.installed = true
 
@@ -17,9 +17,13 @@ const VWave = {
         optionMap.set(el, value ?? {})
 
         el.addEventListener('pointerdown', (event) => {
+          const options = optionMap.get(el)!
+
+          if (options === false) return
+
           wave(event, el, {
             ...globalOptions,
-            ...optionMap.get(el)!
+            ...options
           })
         })
       },
