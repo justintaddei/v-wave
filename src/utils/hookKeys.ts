d021ec9b700eded869867c60c1df017fddc78d75
justintaddei <=>
2022-05-21 01:45:47 -0600

    feat: support local directives
    Resolves #312

--- src/utils/hookKeys.ts
+++ src/utils/hookKeys.ts
@@ -1,8 +1,14 @@
 import { App } from 'vue'
 import { isVue3 } from './isVue3'
 
-const getHooks = (app: App) => {
-  return isVue3(app)
+const getHooks = (app: App | 'vue2' | 'vue3') => {
+  let vue3
+
+  if (app === 'vue2') vue3 = false
+  else if (app === 'vue3') vue3 = true
+  else vue3 = isVue3(app)
+
+  return vue3
     ? {
         mounted: 'mounted',
         updated: 'updated'
