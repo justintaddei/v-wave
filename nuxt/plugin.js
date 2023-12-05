948b802dce428635a6afb956bcd040173a6a0953
justintaddei <=>
2023-12-05 02:40:06 -0700

    WIP: add support for Nuxt 3

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,4 +1,24 @@
-import Vue from 'vue'
 import VWave from 'v-wave'
+import { defineNuxtPlugin, useRuntimeConfig } from '#imports'
 
-Vue.use(VWave, <%= serialize(options) %>)
+export default defineNuxtPlugin((app) => {
+  const options = useRuntimeConfig().public.vWave
+  
+  const directive = options.directive || 'wave'
+
+  const { vWave, vWaveTrigger } = VWave.createLocalWaveDirective(options, app.vueApp)
+
+  app.vueApp.directive(directive, {
+    ...vWave,
+    getSSRProps() {
+      return {}
+    }
+  })
+
+  app.vueApp.directive(`${directive}-trigger`, {
+    ...vWaveTrigger,
+    getSSRProps() {
+      return {}
+    }
+  })
+})
--- nuxt/v3/v-wave.server.ts
+++
@@ -1,6 +0,0 @@
-import { options } from './v-wave.client';
-
-export default defineNuxtPlugin(({ vueApp }) => {
-    vueApp.directive(options.directive ?? 'wave', {});
-    vueApp.directive(`${options.directive ?? 'wave'}-trigger`, {});
-});
