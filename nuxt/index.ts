39e0a9e2a7a0d4f0c9c160248694e540c985c71c
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-12-07 20:23:33 +0100

    refactor: update Nuxt 3 module

---
+++ nuxt/index.ts
@@ -0,0 +1,25 @@
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import type { IVWavePluginOptions } from '../src/options'
+
+type ModuleOptions = Partial<IVWavePluginOptions>
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'v-wave',
+    configKey: 'vWave',
+    compatibility: {
+      nuxt: '^3.0.0',
+      bridge: true
+    }
+  },
+  defaults: {
+    directive: "wave"
+  },
+  setup(options, nuxt) {
+    const { resolve } = createResolver(import.meta.url)
+
+    nuxt.options.runtimeConfig.public.vWave = options
+
+    addPlugin(resolve('./runtime/plugin.ts'))
+  }
+})
